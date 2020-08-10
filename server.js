c65b0d4367b784e2083e15e7d243962032467880
Leonan Muller <leonan.ml97@gmail.com>
2020-08-10 20:46:02 -0300

    api

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
+
+//start app
+const app = express();
+
+//conexão db
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+requireDir('./src/models');
+
+app.use('/api', require('./src/routes'))
+
+app.listen(3003);
